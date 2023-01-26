94c9e43df3be7adbee8cce86afbfe786dc295448
Temka <maksimukart@gmail.com>
2023-01-26 11:42:31 +0100

    done

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if (first == second)
+            System.out.println(first);
+        System.out.println(Math.max(first, second));
+
     }
 }
